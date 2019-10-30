8f455608b512ba9239325c6e35947cd3d68fa36e
Anil Helvacı <anil.helvaci@innology.com.tr>
2019-10-30 04:18:58 +0300

    File reading added.

--- src/FileCipher.java
+++ src/FileCipher.java
@@ -1,15 +1,38 @@
 
 import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.time.Duration;
-import java.time.Instant;
-import java.util.Arrays;
 import java.util.Random;
 
 
-public class Main {
+public class FileCipher {
+
+    public static final String ENCRYPT = "-e";
+    public static final String DECRYPTION = "-d";
+    public static final String CBC = "CBC";
+    public static final String OFB = "OFB";
+    public static final String CTR = "CTR";
+
+    private String operation;
+    private String inputFilePath;
+    private String outputFilePath;
+    private String algorithm;
+    private String mode;
+    private String keyFilePath;
+    private int blockSize;
+
+    public FileCipher(String[] args) {
+        operation = args[0];
+        inputFilePath = args[2];
+        outputFilePath = args[4];
+        algorithm = args[5];
+        mode = args[6];
+        keyFilePath = args[7];
+        blockSize = algorithmBlockSize(algorithm);
+    }
 
     public static byte[] xorla(byte[] iv, byte[] plainTextByte, String algorithm){                                                // Klasik xor.
 
@@ -41,156 +64,57 @@ public class Main {
 
     }
 
-
-    public static byte[] padding(byte[] plainTextBytes, String algorithm){
-
-        int blockSize = algorithmBlockSize(algorithm);
-
-        float boy = (float) plainTextBytes.length/blockSize;
-        int boyWithPadding = (int) Math.ceil(boy)*blockSize;
-
-
-        byte[] plainTextBytesWithPadding = new byte[boyWithPadding];
-        System.arraycopy(plainTextBytes,0,plainTextBytesWithPadding,0,plainTextBytes.length);
-
-
-        byte paddedValue = 32;             //  Mesajın sonunda boşluk ekleme (16 byte ve katı olması için padding) (0x32 = whitespace ASCII kodu)
-
-        for (int i = plainTextBytes.length; i<boyWithPadding; i++){
-
-            plainTextBytesWithPadding[i] = paddedValue;
-
+    private void writeFile(String fileName, byte[] data) {
+        try {
+            FileOutputStream out = new FileOutputStream(fileName);
+            out.write(data);
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        return plainTextBytesWithPadding;
 
 
     }
 
-    public static byte[] deletePadding(byte[] plainTextBytes){
+    public static void main(String[] args) throws NoSuchAlgorithmException, IllegalBlockSizeException, InvalidKeyException, NoSuchPaddingException, BadPaddingException, FileNotFoundException {
 
+        FileCipher fileCipher = new FileCipher(args);
+        ReadFile readFile = new ReadFile(fileCipher.keyFilePath, fileCipher.inputFilePath, fileCipher.algorithm);
 
-        byte paddedValue = 32;
-        int j = plainTextBytes.length;
-        for (int i = plainTextBytes.length-1; i>=0; i--){
-            byte[] temp = new byte[i+1];
-            if (plainTextBytes[i] == paddedValue){
-                j--;
-            }
-            else{
-                System.arraycopy(plainTextBytes,0,temp,0,j);
-                return temp;
-            }
+        ECB ecb = new ECB();
+        CBC cbc = new CBC(fileCipher.blockSize);
+        OFB ofb = new OFB(fileCipher.blockSize);
+        CTR ctr = new CTR(fileCipher.blockSize);
 
+        if (fileCipher.operation.equals(FileCipher.ENCRYPT) && fileCipher.mode.equals(FileCipher.CBC)) {
+            //Todo CBC ENCODE HERE
+            System.out.println("Inside CBC Encode");
+            fileCipher.writeFile(fileCipher.outputFilePath,
+                    cbc.encrypt(readFile.getPaddedText(), readFile.getKey(), ecb, readFile.getInitVec(), fileCipher.algorithm));
+        } else if (fileCipher.operation.equals(FileCipher.ENCRYPT) && fileCipher.mode.equals(FileCipher.OFB)) {
+            //Todo OFB ENCODE HERE
+            fileCipher.writeFile(fileCipher.outputFilePath,
+                    ofb.encrypt(readFile.getPaddedText(), readFile.getKey(), ecb, readFile.getInitVec(), fileCipher.algorithm));
+        } else if (fileCipher.operation.equals(FileCipher.ENCRYPT) && fileCipher.mode.equals(FileCipher.CTR)) {
+            //Todo CTR ENCODE HERE
+            fileCipher.writeFile(fileCipher.outputFilePath,
+                    ctr.encrypt(readFile.getPaddedText(), readFile.getKey(), ecb, readFile.getNonce(), fileCipher.algorithm));
+        } else if (fileCipher.operation.equals(FileCipher.DECRYPTION) && fileCipher.mode.equals(FileCipher.CBC)) {
+            //Todo CBC DECODE HERE
+            System.out.println("Inside CBC Decode");
+            fileCipher.writeFile(fileCipher.outputFilePath,
+                    readFile.deletePadding(cbc.decrypt(readFile.getText(), readFile.getKey(), ecb, readFile.getInitVec(), fileCipher.algorithm)));
+        } else if (fileCipher.operation.equals(FileCipher.DECRYPTION) && fileCipher.mode.equals(FileCipher.OFB)) {
+            //Todo OFB DECODE HERE
+            fileCipher.writeFile(fileCipher.outputFilePath,
+                    readFile.deletePadding(ofb.decrypt(readFile.getText(), readFile.getKey(), ecb, readFile.getInitVec(), fileCipher.algorithm)));
+        } else if (fileCipher.operation.equals(FileCipher.DECRYPTION) && fileCipher.mode.equals(FileCipher.CTR)) {
+            //Todo CTR DECODE HERE
+            fileCipher.writeFile(fileCipher.outputFilePath,
+                    readFile.deletePadding(ctr.decrypt(readFile.getText(), readFile.getKey(), ecb, readFile.getNonce(), fileCipher.algorithm)));
         }
-
-        return "Only Whitespaces can not be send.".getBytes();
-
     }
-
-
-
-    public static void main(String[] args) throws NoSuchAlgorithmException, IllegalBlockSizeException, InvalidKeyException, NoSuchPaddingException, BadPaddingException {
-
-
-        // Key üretimi şimdilik böyle olacak. Example txt dosyaları geldiğinde değişecek.
-
-        //AES key generation
-        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
-        keyGen.init(128);
-        SecretKey secretKey = keyGen.generateKey();
-
-        //DES key generation
-        KeyGenerator keyGen1 = KeyGenerator.getInstance("DES");
-        keyGen1.init(56);
-        SecretKey secretKey1 = keyGen1.generateKey();
-
-
-
-
-        String algorithm = args[3];                         // 3. argüman algoritmayı söyleyecek.
-        int blockSize = algorithmBlockSize(algorithm);
-
-
-        Random rd = new Random(); // creating Random object
-        byte[] iv = new byte[blockSize];
-        for (int i = 0; i < iv.length; i++) {
-            rd.nextBytes(iv); // storing random integers in an array
-        }
-
-        String nonce = "1234";
-
-
-
-        String plainText = "anılın aq";
-        byte[] plainTextByte = plainText.getBytes(StandardCharsets.UTF_8);
-
-
-
-        /*
-
-        ÇOK ÖNEMLİ NOT
-
-        Aşağıdaki denemelerde DES için "secretKey1", AES için "secretKey" parametresi kullanılmalıdır.
-        Hocanın vereceği key algoritmayla uyumlu olaracağı için sadece 1 variabla tutulacaktır. Şu an için hoca input output paylaşmadı.
-        Bu sebepten key'ler farklı variable'larda tutuluyor.
-
-        */
-
-
-
-
-
-
-
-        //@test ECB
-
-
-//        ECB ecb = new ECB();
-//        System.out.println("gönderilecek mesaj->                            "+plainText);
-//        System.out.println("şifreli mesaj->                                 "+new String(ecb.encrypt(padding(plainTextByte,algorithm),secretKey,algorithm)));
-//        System.out.println("şifreli mesajı açınca görülen->                 "+new String(ecb.decrypt(ecb.encrypt(padding(plainTextByte,algorithm),secretKey,algorithm),secretKey,algorithm)));
-
-
-
-
-        // @test CBC
-
-//        CBC cbc = new CBC();
-//        System.out.println("CBC gönderilecek mesaj->               "+new String(plainTextByte));
-//        byte[] sifrelenmisCBC = cbc.encrypt(padding(plainTextByte,algorithm), secretKey, ecb, iv,algorithm);
-//        System.out.println("CBC şifrelenmiş mesaj->                "+new String(sifrelenmisCBC));
-//        System.out.println("CBC şifreli mesajı açınca görülen->    "+new String(cbc.decrypt(sifrelenmisCBC,secretKey,ecb,iv,algorithm)));
-
-
-
-
-        // @test OFB
-
-
-//        OFB ofb = new OFB();
-//        byte[] sifrelenmisOFB = ofb.encrypt(padding(plainTextByte,algorithm),secretKey,ecb,iv,algorithm);
-//        System.out.println("OFB gönderilecek mesaj                     "+new String(plainTextByte));
-//        System.out.println("OFB şifrelenmiş mesaj->                    "+new String(sifrelenmisOFB));
-//        System.out.println("OFB şifreli mesajı açınca görülen->        "+new String(ofb.decrypt(sifrelenmisOFB,secretKey,ecb,iv,algorithm)));
-
-
-
-        //@test CTR
-
-
-//        CTR ctr = new CTR();
-//        String cipherText = new String(ctr.encrypt(padding(plainTextByte,algorithm), secretKey, ecb, nonce,algorithm));
-//        System.out.println(cipherText);
-//        byte[] cipherTextByte = ctr.encrypt(padding(plainTextByte,algorithm), secretKey, ecb, nonce,algorithm);
-//        String plainText1 = new String(deletePadding(ctr.decrypt(cipherTextByte,secretKey,ecb,nonce,algorithm)));
-//        System.out.println(plainText1);
-
-
-    }
-
-
-
-
 
 }
--- src/ReadFile.java
+++ src/ReadFile.java
@@ -1,4 +1,129 @@
-package PACKAGE_NAME;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class ReadFile {
+    private byte[] text;
+    private byte[] paddedText;
+    private byte[] initVec;
+    private SecretKey key;
+    private String nonce;
+
+
+    public ReadFile(String keyFilePath, String inputFilePath, String algorithm) {
+        readKeyFile(keyFilePath, algorithm);
+        readInputFile(inputFilePath);
+        int blockSize = FileCipher.algorithmBlockSize(algorithm);
+        paddedText = padding(text, blockSize);
+    }
+
+    public String readFileAsString(String fileName) {
+        String data = "";
+        try {
+            data = new String(Files.readAllBytes(Paths.get(fileName)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    public byte[] readByteFromFile(String fileName) {
+        File file = new File(fileName);
+        byte[] result = new byte[(int) file.length()];
+        try {
+            FileInputStream fileInputStream = new FileInputStream(fileName);
+            fileInputStream.read(result);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    private void readKeyFile(String filePath, String algorithm) {
+        String[] data = readFileAsString(filePath).split(" - ");
+        initVec = data[0].getBytes(StandardCharsets.UTF_8);
+        byte[] decodedKey = data[1].getBytes(StandardCharsets.UTF_8);
+        System.out.println(decodedKey.length);
+        key = new SecretKeySpec(decodedKey, algorithm);
+        nonce = data[2];
+    }
+
+    public void readInputFile(String filePath) {
+        text = readByteFromFile(filePath);
+    }
+
+    public byte[] getPaddedText() {
+        return paddedText;
+    }
+
+    public static byte[] padding(byte[] plainTextBytes, int blockSize){
+
+        float boy = (float) plainTextBytes.length/blockSize;
+        int boyWithPadding = (int) Math.ceil(boy)*blockSize;
+
+
+        byte[] plainTextBytesWithPadding = new byte[boyWithPadding];
+        System.out.println("PlainTextBytes Size: " + plainTextBytes.length);
+        System.out.println("BoyWithPadding: " + boyWithPadding);
+        System.arraycopy(plainTextBytes,0,plainTextBytesWithPadding,0,plainTextBytes.length);
+
+
+        byte paddedValue = 32;             //  Mesajın sonunda boşluk ekleme (16 byte ve katı olması için padding) (0x32 = whitespace ASCII kodu)
+
+        for (int i = plainTextBytes.length; i<boyWithPadding; i++){
+
+            plainTextBytesWithPadding[i] = paddedValue;
+
+        }
+
+        return plainTextBytesWithPadding;
+
+
+    }
+
+    public byte[] deletePadding(byte[] plainTextBytes){
+
+
+        byte paddedValue = 32;
+        int j = plainTextBytes.length;
+        for (int i = plainTextBytes.length-1; i>=0; i--){
+            byte[] temp = new byte[i+1];
+            if (plainTextBytes[i] == paddedValue){
+                j--;
+            }
+            else{
+                System.arraycopy(plainTextBytes,0,temp,0,j);
+                return temp;
+            }
+
+        }
+
+        return "Only Whitespaces can not be send.".getBytes();
+
+    }
+
+
+    public byte[] getText() {
+        return text;
+    }
+
+    public byte[] getInitVec() {
+        return initVec;
+    }
+
+    public SecretKey getKey() {
+        return key;
+    }
+
+    public String getNonce() {
+        return nonce;
+    }
 }
